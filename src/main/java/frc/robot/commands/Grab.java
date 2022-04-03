5e616e81ddc11818805fc28e9b0c5510bcc5738c
aditya917 <paiaditya@outlook.com>
2022-04-03 16:26:26 -0700

    added new commands and made changes

---
+++ src/main/java/frc/robot/commands/Grab.java
@@ -0,0 +1,48 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Intake;
+
+public class Grab extends CommandBase {
+   /** Creates a new Intake. */
+   private final Intake intake;
+   private final double power;
+   
+   
+   public Grab(Intake intake, double power) {
+     this.intake = intake;
+     this.power = power;
+     addRequirements(intake);
+     
+   }
+ 
+   // Called when the command is initially scheduled.
+   @Override
+   public void initialize() {
+   
+ 
+   }
+ 
+   // Called every time the scheduler runs while the command is scheduled.
+   @Override
+   public void execute() {
+     intake.run(power);
+   }
+ 
+   // Called once the command ends or is interrupted.
+   @Override
+   public void end(boolean interrupted) {
+     intake.stop();
+   }
+ 
+   // Returns true when the command should end.
+   @Override
+   public boolean isFinished() {
+     return !intake.isStopped();
+    
+   }
+ }
